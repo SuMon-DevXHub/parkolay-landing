aa38b2831dc8517c8d2be5f6e17700ff01f24b5d
sumoncse19 <mdsumoncse19@gmail.com>
2024-11-21 19:28:06 +0600

    sub product page

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,6 +6,22 @@ import YouTube from "../../assets/images/youTube.svg";
 import Instagram from "../../assets/images/instagram.svg";
 import Twitter from "../../assets/images/twitter.svg";
 import Linkedin from "../../assets/images/linkedin.svg";
+import { HeadFC, Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
+
+const footerSchema = {
+  "@context": "https://schema.org",
+  "@type": "Product",
+  name: "Parkolay, Parking Solutions",
+  description: "Space-saving, efficient car parking solution by parkolay",
+  category: "Parking Solutions",
+  manufacturer: {
+    "@type": "Organization",
+    name: "Parkolay",
+    url: "https://parkolay.com"
+  }
+};
+
 const Footer: React.FC = () => {
   return (
     <footer className="bg-[#383E42] text-white pt-12 2xl:mt-28 xl:mt-24 md:mt-16 mt-12 w-full">
@@ -16,7 +32,7 @@ const Footer: React.FC = () => {
               Office Address
             </h2>
             <p className="font-semibold xl:text-xl text-lg xl:mt-4 mt-2 xl:mb-2 mb-1">
-              Headquarter & Factory
+              HEADQUARTER & FACTORY
             </p>
             <p className="font-normal text-sm">
               Merkez Mah. Sardunya Cad. No:8,
@@ -26,7 +42,7 @@ const Footer: React.FC = () => {
           </div>
 
           <div className="flex justify-center md:items-center items-start xl:gap-5 gap-3 flex-col">
-            <img
+            <StaticImage
               src={ParkolayLogo}
               alt="Parkolay Logo"
               className="md:mx-auto max-w-[240px] w-full"
@@ -35,7 +51,7 @@ const Footer: React.FC = () => {
               Get started to up your business
             </p>
             <div className="flex items-center justify-center">
-              <img
+              <StaticImage
                 src={Captcha}
                 alt="parkolay captcha"
                 className="w-16 h-16 md:hidden lg:block block"
@@ -61,41 +77,41 @@ const Footer: React.FC = () => {
             <p className="text-sm font-normal">Phone: +90 (850) 333 36 67</p>
             <p className="text-sm font-normal">Fax: +90 (226) 353 27 74</p>
             <div className="flex justify-start items-center gap-2 md:mt-8 mt-6">
-              <a href="#" className="">
-                <img
+              <Link to="https://www.facebook.com/otomatikotopark" className="">
+                <StaticImage
                   src={Facebook}
                   alt="parkolay facebook link"
                   className="w-8 h-8"
                 />
-              </a>
-              <a href="#" className="">
-                <img
+              </Link>
+              <Link to="https://www.youtube.com/user/otomatikotopark" className="">
+                <StaticImage
                   src={YouTube}
-                  alt="parkolay facebook link"
+                  alt="parkolay youtube link"
                   className="w-8 h-8"
                 />
-              </a>
-              <a href="#" className="">
-                <img
+              </Link>
+              <Link to="https://www.instagram.com/parkolay_otomatik_otopark/" className="">
+                <StaticImage
                   src={Instagram}
-                  alt="parkolay facebook link"
+                  alt="parkolay instagram link"
                   className="w-8 h-8"
                 />
-              </a>
-              <a href="#" className="">
-                <img
+              </Link>
+              <Link to="https://www.instagram.com/parkolay_otomatik_otopark/" className="">
+                <StaticImage
                   src={Twitter}
-                  alt="parkolay facebook link"
+                  alt="parkolay twitter link"
                   className="w-8 h-8"
                 />
-              </a>
-              <a href="#" className="">
-                <img
+              </Link>
+              <Link to="https://www.linkedin.com/company/parkolay/" className="">
+                <StaticImage
                   src={Linkedin}
-                  alt="parkolay facebook link"
+                  alt="parkolay linkedin link"
                   className="w-8 h-8"
                 />
-              </a>
+              </Link>
             </div>
           </div>
         </div>
@@ -104,19 +120,19 @@ const Footer: React.FC = () => {
           <div className="md:px-10 px-6 xl:px-20 flex justify-between items-center py-4 md:flex-row flex-col max-w-[1920px] w-full mx-auto">
             <p>
               &copy; 2024.{" "}
-              <a href="#" className="text-[#05B6C7]">
+              <Link to="#" className="text-[#05B6C7]">
                 Parkolay Company Inc.
-              </a>{" "}
+              </Link>{" "}
               All Rights Reserved.
             </p>
             <div className="flex justify-center space-x-4 mt-2">
-              <a href="#" className="text-[#05B6C7]">
+              <Link to="#" className="text-[#05B6C7]">
                 Privacy & Policy
-              </a>
+              </Link>
               <span>|</span>
-              <a href="#" className="text-[#05B6C7]">
+              <Link to="#" className="text-[#05B6C7]">
                 Terms of Use
-              </a>
+              </Link>
             </div>
           </div>
           <div className="h-2 w-full bg-[#05B6C7]"></div>
@@ -127,3 +143,13 @@ const Footer: React.FC = () => {
 };
 
 export default Footer;
+
+export const Head: HeadFC = () => (
+  <>
+    <title>Car Lift Systems | Parkolay</title>
+    <meta name="description" content="Space-saving, efficient car parking solution by parkolay" />
+    <script type="application/ld+json">
+      {JSON.stringify(footerSchema)}
+    </script>
+  </>
+);
