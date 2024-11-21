360c4c5f6534f802f2fb289936a6c9323dfe2318
sumoncse19 <mdsumoncse19@gmail.com>
2024-11-21 20:34:06 +0600

    Footer issue fixed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,27 +1,4 @@
 import React from "react";
-import ParkolayLogo from "../../assets/images/logo/parkolay_logo.svg";
-import Captcha from "../../assets/images/captcha.svg";
-import Facebook from "../../assets/images/facebook.svg";
-import YouTube from "../../assets/images/youTube.svg";
-import Instagram from "../../assets/images/instagram.svg";
-import Twitter from "../../assets/images/twitter.svg";
-import Linkedin from "../../assets/images/linkedin.svg";
-import { HeadFC, Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
-
-const footerSchema = {
-  "@context": "https://schema.org",
-  "@type": "Product",
-  name: "Parkolay, Parking Solutions",
-  description: "Space-saving, efficient car parking solution by parkolay",
-  category: "Parking Solutions",
-  manufacturer: {
-    "@type": "Organization",
-    name: "Parkolay",
-    url: "https://parkolay.com"
-  }
-};
-
 import { HeadFC, Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 
@@ -41,7 +18,6 @@ const footerSchema = {
 const Footer: React.FC = () => {
   return (
     <footer className="bg-[#383E42] text-white pt-12 2xl:mt-28 xl:mt-24 md:mt-16 mt-12 w-full">
-    <footer className="bg-[#383E42] text-white pt-12 2xl:mt-28 xl:mt-24 md:mt-16 mt-12 w-full">
       <div className="mx-auto block">
         <div className="max-w-[1920px] w-full md:px-10 px-6 xl:px-20  grid md:grid-cols-3 grid-cols-1 md:justify-items-stretch justify-items-start xl:gap-8 md:gap-4 gap-8 mx-auto">
           <div className="">
@@ -49,7 +25,6 @@ const Footer: React.FC = () => {
               Office Address
             </h2>
             <p className="font-semibold xl:text-xl text-lg xl:mt-4 mt-2 xl:mb-2 mb-1">
-              HEADQUARTER & FACTORY
               HEADQUARTER & FACTORY
             </p>
             <p className="font-normal text-sm">
@@ -162,7 +137,6 @@ const Footer: React.FC = () => {
                   quality={95}
                 />
               </Link>
-              </Link>
             </div>
           </div>
         </div>
@@ -172,23 +146,17 @@ const Footer: React.FC = () => {
             <p>
               &copy; 2024.{" "}
               <Link to="#" className="text-[#05B6C7]">
-              <Link to="#" className="text-[#05B6C7]">
                 Parkolay Company Inc.
-              </Link>{" "}
               </Link>{" "}
               All Rights Reserved.
             </p>
             <div className="flex justify-center space-x-4 mt-2">
               <Link to="#" className="text-[#05B6C7]">
-              <Link to="#" className="text-[#05B6C7]">
                 Privacy & Policy
-              </Link>
               </Link>
               <span>|</span>
               <Link to="#" className="text-[#05B6C7]">
-              <Link to="#" className="text-[#05B6C7]">
                 Terms of Use
-              </Link>
               </Link>
             </div>
           </div>
@@ -200,16 +168,6 @@ const Footer: React.FC = () => {
 };
 
 export default Footer;
-
-export const Head: HeadFC = () => (
-  <>
-    <title>Car Lift Systems | Parkolay</title>
-    <meta name="description" content="Space-saving, efficient car parking solution by parkolay" />
-    <script type="application/ld+json">
-      {JSON.stringify(footerSchema)}
-    </script>
-  </>
-);
 
 export const Head: HeadFC = () => (
   <>
